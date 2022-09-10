028c048ca5f8c9b45f8fca82947453b10ec2f9da
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-10 07:01:43 +0000

    Task_A_4_0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - "serve as major entry point where printing is done".
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
