ef9164c23cf3d27e90cffb8a790df321d6cc5107
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-11 22:56:02 +0000

    TASKS_B_6_0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints numbers between 0 and 10
+ * Return: 0;
+ */
+int main(void)
+{
+        int number;
+
+        for(number = 0; number < 10; number++)
+                putchar(number);
+        putchar('\n');
+
+        return (0);
+}
