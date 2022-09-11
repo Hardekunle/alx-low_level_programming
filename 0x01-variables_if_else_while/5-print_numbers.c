bbab2e4bc3ed02b6d1609c915825090d2071667b
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-11 22:53:13 +0000

    TASKS_B_5_0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints numbers between 0 and 10
+ * Return: 0;
+ */
+int main(void)
+{
+        int number;
+
+        for(number = 0; number < 10; number++)
+                printf("%d", number);
+        printf('\n');
+
+        return (0);
+}
