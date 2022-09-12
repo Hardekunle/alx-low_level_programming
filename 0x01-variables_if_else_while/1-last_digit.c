270dd4588293565783cb7fd329d3c8caca1a81f4
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-12 00:10:26 +0000

    lass digit solution

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,23 +7,21 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 	int quotient;
 	int lastNo;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-	quotient = n/10;
-	lastNo = n - (quotient * 10);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastNo = n % 10;
 
-	printf("Last digit of %d is %d ", n, quotient);	
-        
+	printf("Last digit of %d is %d ", n, lastNo);
+
 	if (lastNo == 0)
 		printf("and is 0\n");
 	else if (lastNo > 5)
-                printf("and is greater than 5\n");
+		printf("and is greater than 5\n");
 	else
-                printf("and is less than 6 and not 0\n");
-        
-        return (0);
+		printf("and is less than 6 and not 0\n");
+	return (0);
 }
