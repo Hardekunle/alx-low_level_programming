9f2e32beece50c8227efd18b1c68e1357d170084
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-17 00:11:57 +0000

    Task_F_9_0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *FizzBuzz- print words dynamically
+ *Return: void
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			printf("FizzBuzz");
+		else if (number % 3 == 0)
+			printf("Fizz");
+		else if (number % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", number);
+
+		if (number < 100)
+			printf(" ");
+
+	}
+	printf("\n");
+	return (0);
+}
