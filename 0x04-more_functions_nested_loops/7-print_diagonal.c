e2d41bbed55c9d19d94c25d0601204793f544376
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-17 04:30:11 +0000

    Task_F_4_0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagonal- prints diangonal for a given length
+ *@n: size of diagonal
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+	int whiteSpace;
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 1; i <= n; i++)
+	{
+		whiteSpace = i - 1;
+		while (whiteSpace > 0)
+		{
+			_putchar(' ');
+			whiteSpace--;
+		}
+
+		_putchar('\');
+		_putchar('\n');
+
+	}
+}
