963df592a8b1233b1b7af3b68986521cc9662cfa
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-19 23:56:59 +0000

    Task_G_2_0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strlen- calculates the length of the string
+ *@s: pointer variable to first element in array s
+ *Return: int
+ */
+int _strlen(char *s)
+{
+	int number;
+
+	number = strlen(*s);
+	return (number);
+}
