037cfc6df4ba4f34a40788ad8f5a3e918cd9f24e
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-20 01:35:25 +0000

    Task_G_8_0

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array- prints the value of elements in array
+ *@a: pointer variable to first element
+ *@n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	for (i = 0; i < n ; i++)
+	{
+		printf(*(a + i));
+		if (i != n - 1)
+			printf(", ");
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+void print_array(int *a, int n);
 void print_rev(char *s);
 int _atoi(char *s);
