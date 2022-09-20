b9a75dafc4a47e22f8664a70987c367c32c49415
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-20 01:24:54 +0000

    Task_G_7_1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,12 @@ void puts_half(char *str)
 		val = *(str + len);
 	}
 
-	start = (len / 2) - 1;
+
+	if ( len % 2 == 1)
+		start = (len / 2) + 1;
+	else
+		start = (len / 2);
+
 	for (i = start; i < len; i++)
 	{
 		_putchar(*(str + i));
