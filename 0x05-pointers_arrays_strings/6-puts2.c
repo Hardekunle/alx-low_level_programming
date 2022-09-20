bc2074506d74237463e94015014e8d0c91ee9e3b
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-20 01:01:41 +0000

    Task_G_6_0

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints character in even postion.
+ * @str: string.
+ */
+void puts2(char *str)
+{
+	int len;
+	int i;
+
+	len = 0;
+	while (val != '\0')
+	{
+		len++;
+		val = *(s + len);
+	}
+
+	i = 0;
+	while (i < len)
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+
+	_putchar('\n');
+}
+~
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 void rev_string(char *s);
+void puts2(char *str);
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
