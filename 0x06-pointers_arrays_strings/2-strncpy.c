9c46dd3e5733343cb2fc17ffee47394fcdba49aa
Hardekunle <adekunlefolorunso80@gmail.com>
2022-09-23 00:11:55 +0000

    Tasks_H_3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include"holberton.h"
+
+/**
+ * _strncpy - copy string function
+ * @src: pointer to first string
+ * @dest: pointer of second string
+ * @n: integer
+ * Return: string copy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+
+
+	}
+	return (dest);
+}
